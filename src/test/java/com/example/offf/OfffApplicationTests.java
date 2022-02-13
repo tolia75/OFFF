030326594177c5e905dee83d6cf828fe48abb05e
tolia75 <46533144+tolia75@users.noreply.github.com>
2022-02-13 15:31:23 +0100

    Initial commit

---
+++ src/test/java/com/example/offf/OfffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.offf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
